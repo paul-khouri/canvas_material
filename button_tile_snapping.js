7839739bd4fb133350a179da1040ddc2132e2016
paul-khouri <45544400+paul-khouri@users.noreply.github.com>
2018-12-16 12:42:31 +1100

    Add files via upload

--- button_tile_snapping.js
+++ button_tile_snapping.js
@@ -29,6 +29,13 @@ var ctx = setupCanvas(document.querySelector('.myCanvas'));
 canvas = document.querySelector('.myCanvas');
 const width = canvas.width/myScale;
 const height = canvas.height/myScale;
+// images
+var imgCircle= new Image();
+imgCircle.src="images/circle_button.png";
+var imgSquare= new Image();
+imgSquare.src="images/square_button.png";
+var imgTriangle= new Image();
+imgTriangle.src="images/triangle_button.png";
 
 
 
@@ -56,10 +63,10 @@ var selectedButton;
 //------------------------------
 class Button{
     // string name, int x, int y, int w,int h, stringrgba bC, stringrgba hC, stringrgba sC,boolean s  
-   constructor(name,x,y,w,h,bC, hC, sC){
+   constructor(name,x,y,w,h,bC, hC, sC, pic_name){
        this.name = name; this.x=x; this.y=y; this.width = w; this.height = h;
        this.baseColor = bC; this.hoverColor = hC ; this.selectedColor= sC ;
-       this.selected = false;  
+       this.selected = false; this.pic = pic_name;  
    }
    update(){
        if(this.inBounds(xMouse, yMouse, this.x, this.y, this.width, this.height)){
@@ -80,18 +87,14 @@ class Button{
        this.draw();
    }
    draw(){
-       ctx.strokeStyle = "rgb(255,255,153)";
-       ctx.lineWidth = 2;
-       ctx.beginPath();
-       ctx.rect(this.x, this.y, this.width, this.height)
-       ctx.fill();
-       ctx.stroke();
-       ctx.fillStyle="rgb(255,255,255)";
-        var myFont= "30px monospace";
-        ctx.font=myFont;
-        ctx.textBaseline='middle';
-        ctx.textAlign='center';
-        ctx.fillText(this.name, this.x+this.width/2,this.y+2*this.height/4);
+    ctx.strokeStyle = "rgb(255,255,153)";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height)
+    ctx.fill();
+    ctx.stroke();
+    ctx.fillText(this.name, this.x+this.width/2,this.y+2*this.height/4);
+    ctx.drawImage(this.pic, this.x, this.y, 100, 100);
    }
    }
    Button.prototype.inBounds=inBounds;
@@ -118,7 +121,8 @@ class Circle{
         ctx.fillStyle = this.f_col;
         ctx.strokeStyle = this.s_col;
         ctx.beginPath()
-        ctx.arc(this.x,this.y, this.r, 0, 2*Math.PI);
+        ctx.arc(this.x,this.y, this.r, 0, 2*Math.PI,false);
+        ctx.arc(this.x,this.y,this.r/2,0,Math.PI*2, true);
     if(this.f){
         ctx.fill();
     }
@@ -154,7 +158,55 @@ class Rectangle{
         ctx.rect(this.x,this.y,this.w,this.h)
         ctx.closePath();
         ctx.fill();
+        ctx.fillStyle = this.s_col;
+        ctx.strokeStyle = this.f_col;
+        ctx.beginPath();
+        ctx.arc(this.x+this.w/2, this.y+this.h/2, this.h/4, 0,2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+        ctx.strokeStyle = this.s_col;
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x+this.w, this.y+this.h);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(this.x+this.w, this.y);
+        ctx.lineTo(this.x, this.y+this.h);
+        ctx.stroke();
         //ctx.stroke();
+        
+    }
+}
+// Triangle x, y, width, height, fillcolour stroke colour, fill(b), stroke(b), l
+class Triangle{
+    constructor(x,y,w,h,f_col,s_col, f,s,l){
+        this.x=x+l;
+        this.y=y+l;
+        this.w=w-2*l;
+        this.h=h-2*l;
+        this.f_col=f_col;
+        this.s_col = s_col;
+        this.f=f;
+        this.s=s;
+        this.l=l;
+    }
+    update(){
+        this.draw();
+
+    }
+    draw(){
+        ctx.fillStyle = this.f_col;
+        ctx.strokeStyle = this.s_col;
+        ctx.lineWidth = this.l;
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x+this.w, this.y+this.h);
+        ctx.lineTo(this.x,this.y+this.h);
+        ctx.lineTo(this.x+this.w, this.y);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+
         
     }
 }
@@ -225,12 +277,13 @@ var boundryHeight = 400;
 //deep green blue, green, pale green
 //red, yellow, blue black
 var mycol = ["rgb(255,255,255)", "rgb(153,153,153)", "rgb(0,0,0)",
-"rgb(11,73,88)", "rbg(77,138,124)", "rgb(255,243,139)", 
+"rgb(11,73,88)", "rgb(77,138,124)", "rgb(196,219,148)", 
 "rgb(255,78,100)", "rgb(255,255,153)" , "rgb(27,63,72)"];
-myGrid = new Grid(boundryX,boundryY,boundryWidth, boundryHeight, 10, 10,"rgb(255,255,255)", 0.5);
+myGrid = new Grid(boundryX,boundryY,boundryWidth, boundryHeight, 5, 5,"rgb(255,255,255)", 0.5);
 // string name, int x, int y, int w,int h, stringrgba bC, stringrgba hC, stringrgba sC,boolean s 
-squareButton = new Button("Square",25,450, 133,60,"rgb(51,51,255)", "rgb(255,255,153)", "rgb(0,153,204)" );
-circleButton = new Button("Circle",158,450, 133,60,"rgb(51,51,255)", "rgb(255,255,153)", "rgb(0,153,204)" );
+squareButton = new Button("Square",25,450, 100,100,"rgb(51,51,255)", "rgb(255,255,153)", "rgb(0,153,204)", imgSquare );
+circleButton = new Button("Circle",125,450, 100,100,"rgb(51,51,255)", "rgb(255,255,153)", "rgb(0,153,204)", imgCircle );
+triangleButton = new Button("Triangle",225,450, 100,100,"rgb(51,51,255)", "rgb(255,255,153)", "rgb(0,153,204)", imgTriangle );
 
 var count = 0;
 function animate(){
@@ -239,6 +292,7 @@ function animate(){
     myGrid.update();
     squareButton.update();
     circleButton.update();
+    triangleButton.update();
     for(var k=0; k<objectSet.length; k++){
         objectSet[k].update();
         
@@ -248,20 +302,25 @@ function animate(){
         console.log("mouse in bounds");
 
     if(mouseDown && selectedButton){
-        var xRound = roundTo(xMouse, 40,25);
-        var yRound = roundTo(yMouse, 40,25);
+        var xRound = roundTo(xMouse, 80,25);
+        var yRound = roundTo(yMouse, 80,25);
         if(selectedButton.name == "Square"){
             console.log("I want to draw a square");
             // Rectangle x, y, width, height, fillcolour stroke colour, fill(b), stroke(b), l
-            objectSet.push(new Rectangle(xRound, yRound, 40,40,"rgb(204,204,0)","rgb(200,200,200)", true, false, 0.1 ))
+            objectSet.push(new Rectangle(xRound, yRound, 80,80, mycol[5] ,mycol[6], true, false, 2 ))
             mouseDown = false;
         }
         if(selectedButton.name == "Circle"){
             console.log("I want to draw a circle");
             // Circle x, y,radius, fillcolour stroke colour, fill(b), stroke(b), l
-            objectSet.push(new Circle(xRound+20,yRound+20,20,"rgb(200,200,200)","rgb(204,0,0)",true,false,0.1))
+            objectSet.push(new Circle(xRound+40,yRound+40,40,"rgb(200,200,200)","rgb(204,0,0)",true,false,0.1))
             mouseDown = false;
-
+        }
+        if(selectedButton.name == "Triangle"){
+            console.log("I want to draw a Triangle");
+            // Triangle x, y, width, height, fillcolour stroke colour, fill(b), stroke(b), l
+            objectSet.push(new Triangle(xRound,yRound,80,80,mycol[6],mycol[7],true,true,5))
+            mouseDown = false;
         }
     }
 }
